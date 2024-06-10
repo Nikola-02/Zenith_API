a17e18249fbf3f4a280375074ba340f4808b24f0
Nikola <nikoladjunisic@gmail.com>
2024-06-10 16:25:48 +0200

    Added context

--- Zenith_API.DataAccess/ZenithContext.cs
+++ Zenith_API.DataAccess/ZenithContext.cs
@@ -1,7 +1,62 @@
-﻿namespace Zenith_API.DataAccess
-{
-    public class ZenithContext
-    {
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Zenith_API.Domain;
 
+namespace Zenith_API.DataAccess
+{
+    public class ZenithContext : DbContext
+    {
+        private readonly string _connectionString;
+
+        public ZenithContext(string connectionString)
+        {
+            this._connectionString = connectionString;
+        }
+
+        public ZenithContext()
+        {
+            this._connectionString = "Data Source=DESKTOP-CG6HMPT\\SQLEXPRESS;Initial Catalog=Zenith;TrustServerCertificate=true;Integrated security = true";
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_connectionString).UseLazyLoadingProxies();
+
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(this.GetType().Assembly);
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public override int SaveChanges()
+        {
+            IEnumerable<EntityEntry> entries = this.ChangeTracker.Entries();
+
+            foreach (EntityEntry entry in entries)
+            {
+                if (entry.State == EntityState.Added)
+                {
+                    if (entry.Entity is Entity e)
+                    {
+                        e.IsActive = true;
+                        e.CreatedAt = DateTime.UtcNow;
+                    }
+                }
+
+                if (entry.State == EntityState.Modified)
+                {
+                    if (entry.Entity is Entity e)
+                    {
+                        e.UpdatedAt = DateTime.UtcNow;
+                    }
+                }
+            }
+
+            return base.SaveChanges();
+        }
     }
 }
