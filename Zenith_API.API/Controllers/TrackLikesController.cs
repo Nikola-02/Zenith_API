2caa7296022ef7b6c5fcdb0e75da0a1fdb1b3970
Nikola-02 <nikoladjunisic@gmail.com>
2024-06-23 12:26:28 +0200

    Added like exists

--- Zenith_API.API/Controllers/TrackLikesController.cs
+++ Zenith_API.API/Controllers/TrackLikesController.cs
@@ -1,9 +1,11 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Zenith_API.Application;
 using Zenith_API.Application.DTO.TrackLikes;
 using Zenith_API.Application.DTO.Tracks;
 using Zenith_API.Application.UseCases.Commands.TrackLikes;
 using Zenith_API.Application.UseCases.Commands.Tracks;
+using Zenith_API.DataAccess;
 using Zenith_API.Implementation;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -15,10 +17,26 @@ namespace Zenith_API.API.Controllers
     public class TrackLikesController : ControllerBase
     {
         private UseCaseHandler _handler;
+        private ZenithContext _context;
+        private IApplicationActor _actor;
 
-        public TrackLikesController(UseCaseHandler handler)
+        public TrackLikesController(UseCaseHandler handler, ZenithContext context, IApplicationActor actor)
         {
             _handler = handler;
+            _context = context;
+            _actor = actor;
+        }
+
+        // GET api/<TrackLikesController>/trackId
+        [Authorize]
+        [HttpGet("{trackId}")]
+        public IActionResult Get(int trackId)
+        {
+            var like = _context.Likes.FirstOrDefault(x=>x.TrackId == trackId && x.UserId == _actor.Id);
+
+            var isLiked = like != null;
+
+            return Ok(new { exists = isLiked });
         }
 
         // POST api/<TrackLikesController>
